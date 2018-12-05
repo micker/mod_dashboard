3876a073631985852fe5026238dc89407233b695
Berges Yannick <micker@free.fr>
2018-12-05 22:33:29 +0100

    first step for iconfield element

---
+++ elements/iconpicker.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * @package     Joomla.Platform
+ * @subpackage  Form
+ *
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('JPATH_PLATFORM') or die;
+
+jimport('joomla.filesystem.folder');  // JFolder
+jimport('joomla.filesystem.file');    // JFile
+
+jimport('cms.html.html');      // JHtml
+jimport('cms.html.select');    // JHtmlSelect
+
+jimport('joomla.form.helper'); // JFormHelper
+JFormHelper::loadFieldClass('list');   // JFormFieldList
+
+class JFormFieldIconpicker extends JFormField {
+                       protected $type = 'Iconpicker';
+	// getLabel() left out
+	public function getInput() {
+                       JHtml::_('jquery.framework');
+                       JHtml::_('bootstrap.framework');
+                       JHtml::_('stylesheet', 'modules/mod_joomadmin/assets/css/bootstrap-iconpicker.css');
+                       JHtml::_('stylesheet', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+                       JHtml::_('script', 'modules/mod_joomadmin/assets/js/bootstrap-iconpicker-iconset-all.js');
+                       JHtml::_('script', 'modules/mod_joomadmin/assets/js/bootstrap-iconpicker.js');
+                       $iconlist = ' <div id="'. $this->id .'-wrapper" /></div>';
+                       $iconlist .="
+                       <script>
+                       (function ($) {
+                       $('#". $this->id ."-wrapper').iconpicker({
+                       align: 'left',
+                       arrowClass: 'btn-success',
+                       arrowPrevIconClass: 'fa fa-arrow-left',
+                       arrowNextIconClass: 'fa fa-arrow-right',
+                       cols: 5,
+                       rows:2,
+                       footer: true,
+                       header: true,
+                       iconset: 'fontawesome',
+                       labelHeader: '" . JText::sprintf( 'PLG_FIELDS_ICONLINK_PAGESINDEX', '{0}', '{1}' ) . "',
+                       labelFooter: '" . JText::sprintf( 'PLG_FIELDS_ICONLINK_ICONSINDEX', '{0}', '{1}', '{2}' ) . "',
+                       placement: 'bottom',
+                       search: true,
+                       searchText: '". JText::_('PLG_FIELDS_ICONLINK_SEARCHTEXT') . "',
+                       selectedClass: 'btn-primary',
+                       unselectedClass: 'btn-default',
+                       iconClass: 'fontawesome',
+                       iconClassFix: 'fa fa-'
+                       });
+
+                       var myfield = $('#" . $this->id . "-wrapper'),
+                       input = $('input', myfield);
+                       input.attr({'id': '" . $this->id . "', 'name': '" . $this->name . "'});
+                       input.val('" . $this->value . "');
+
+                       })(jQuery);
+                       </script>";
+
+                       return $iconlist;
+                       }
+}
