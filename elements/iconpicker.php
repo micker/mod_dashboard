a4c62a6c9ff03dd3f14559a04fe96dfc0d604158
Berges Yannick <yannick@com3elles.com>
2026-08-04 15:15:42 +0200

    Refactor icon picker to use HTMLHelper for j6

--- elements/iconpicker.php
+++ elements/iconpicker.php
@@ -16,11 +16,8 @@
 
 defined('_JEXEC') or die;
 
-use Joomla\CMS\Factory;
 use Joomla\CMS\Form\FormField;
 use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Uri\Uri;
 
 /**
  * Form Field class for icon picker
@@ -46,97 +43,47 @@ class JFormFieldIconpicker extends FormField
      */
     protected function getInput()
     {
-        $doc = Factory::getApplication()->getDocument();
-        
+       
         // Charger les assets CSS
-        $doc->addStyleSheet(Uri::root(true) . '/media/mod_dashboard/css/style.css');
-        $doc->addStyleSheet(Uri::root(true) . '/media/mod_dashboard/css/font-awesome.min.css');
+        HTMLHelper::_('stylesheet', 'media/mod_dashboard/css/style.css');
+        HTMLHelper::_('stylesheet', 'media/mod_dashboard/css/font-awesome.min.css');
         
         // Charger le script JS principal
-        $doc->addScript(Uri::root(true) . '/media/mod_dashboard/js/universal-icon-picker.min.js');
+       HTMLHelper::_('script', 'media/mod_dashboard/js/universal-icon-picker.min.js');
 
-        // Utiliser WAI-ARIA pour l'accessibilité
-        $iconlist = '<div class="input-group mb-3">';
-        $iconlist .= '    <span class="input-group-text" id="' . $this->id . '-icon" aria-label="' . Text::_('MOD_DASHBOARD_ICON_PREVIEW') . '">';
-        $iconlist .= '        <i class="fa ' . htmlspecialchars($this->value, ENT_QUOTES, 'UTF-8') . '"></i>';
-        $iconlist .= '    </span>';
-        $iconlist .= '    <input type="text" ';
-        $iconlist .= '        id="' . $this->id . '-wrapper" ';
-        $iconlist .= '        value="' . htmlspecialchars($this->value, ENT_QUOTES, 'UTF-8') . '" ';
-        $iconlist .= '        name="' . $this->name . '-wrapper" ';
-        $iconlist .= '        class="form-control" ';
-        $iconlist .= '        aria-describedby="' . $this->id . '-icon" />';
-        $iconlist .= '    <button type="button" ';
-        $iconlist .= '        id="' . $this->id . '-clear" ';
-        $iconlist .= '        class="btn btn-outline-secondary">';
-        $iconlist .= '        ' . Text::_('JCLEAR') . '';
-        $iconlist .= '    </button>';
-        $iconlist .= '</div>';
+        $id    = htmlspecialchars($this->id, ENT_QUOTES, 'UTF-8');
+    $name  = htmlspecialchars($this->name, ENT_QUOTES, 'UTF-8');
+    $value = htmlspecialchars((string) $this->value, ENT_QUOTES, 'UTF-8');
 
-        // Ajouter le JavaScript inline avec timeout maximum
-        $script = "
-(function() {
-    let attempts = 0;
-    const maxAttempts = 50; // 5 secondes maximum (50 * 100ms)
-    
-    function initIconPicker() {
-        attempts++;
-        
-        if (typeof UniversalIconPicker === 'undefined') {
-            if (attempts < maxAttempts) {
-                setTimeout(initIconPicker, 100);
-            } else {
-                console.error('UniversalIconPicker library failed to load after ' + (maxAttempts * 100 / 1000) + ' seconds');
-                console.error('Please check if the file exists at: ' + '" . Uri::root(true) . "/media/mod_dashboard/js/universal-icon-picker.min.js');
+    $iconlist = ' <div class="input-group mb-3">
+    <span class="input-group-text" id="' . $id . '-icon">
+    <i class="fa '.$value.'"></i>
+    </span>
+    <input id="' . $id . '-wrapper" value="'.$value.'" name="' . $name . '-wrapper"  class="form-control"/><button type="button" id="' . $id . '-clear" class="btn btn-outline-secondary">
+    Reset
+    </button></div>';
+    $iconlist .= "
+    <script>
+        document.addEventListener('DOMContentLoaded', function(event) {
+        var uip = new UniversalIconPicker('#" . $id . "-wrapper', {
+            iconLibraries: [
+              'font-awesome.min.json'
+            ],
+            iconLibrariesCss: [
+            '../../../media/mod_flexiadmin/css/font-awesome.min.css'
+            ],
+            resetSelector: '#" . $id . "-clear',  // must be an ID or '' if no reset button
+            onSelect: function(jsonIconData) {
+            document.getElementById('" . $id . "-wrapper').value = jsonIconData.iconClass;
+            document.getElementById('" . $id . "-icon').innerHTML = jsonIconData.iconHtml;
+            },
+            onReset: function() {
+              document.getElementById('" . $id . "-wrapper').value = '';
             }
-            return;
-        }
-        
-        try {
-            const uip = new UniversalIconPicker('#" . $this->id . "-wrapper', {
-                iconLibraries: [
-                    'font-awesome.min.json'
-                ],
-                iconLibrariesCss: [
-                    '" . Uri::root(true) . "/media/mod_dashboard/css/font-awesome.min.css'
-                ],
-                resetSelector: '#" . $this->id . "-clear',
-                onSelect: function(jsonIconData) {
-                    const wrapperInput = document.getElementById('" . $this->id . "-wrapper');
-                    const iconSpan = document.getElementById('" . $this->id . "-icon');
-                    if (wrapperInput) {
-                        wrapperInput.value = jsonIconData.iconClass;
-                    }
-                    if (iconSpan) {
-                        iconSpan.innerHTML = jsonIconData.iconHtml;
-                    }
-                },
-                onReset: function() {
-                    const wrapperInput = document.getElementById('" . $this->id . "-wrapper');
-                    const iconSpan = document.getElementById('" . $this->id . "-icon');
-                    if (wrapperInput) {
-                        wrapperInput.value = '';
-                    }
-                    if (iconSpan) {
-                        iconSpan.innerHTML = '<i class=\"fa\"></i>';
-                    }
-                }
             });
-            console.log('UniversalIconPicker initialized successfully for field: " . $this->id . "');
-        } catch (error) {
-            console.error('Error initializing UniversalIconPicker:', error);
-        }
-    }
-    
-    // Attendre que la page soit complètement chargée
-    window.addEventListener('load', function() {
-        setTimeout(initIconPicker, 100);
-    });
-})();
-";
-
-        $doc->addScriptDeclaration($script);
-
-        return $iconlist;
-    }
+        });
+    </script>
+ ";
+    return $iconlist;
+  }
 }
